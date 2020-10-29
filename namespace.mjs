7288318621d1873d7af0b7b3a658a7b7b21e1d79
papiando <info@papiando.com>
2020-10-28 20:18:19 -0400

    Save progress

--- namespace.mjs
+++ namespace.mjs
@@ -1,4 +1,5 @@
 import Application from './lib/Application.mjs';
 import Core from './lib/Core.mjs';
+import Router from './lib/Router.mjs';
 
-export default { Application, Core };
+export default { Application, Core, Router };
