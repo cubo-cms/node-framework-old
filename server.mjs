b6580f183e1250d3275dc575026ccb5b4df6d01b
papiando <info@papiando.com>
2020-11-01 16:31:26 -0400

    Main server.mjs

---
+++ server.mjs
@@ -0,0 +1,17 @@
+/** @package        @cubo-cms/node-framework
+  * @module         /server.mjs
+  * @version        0.3.20
+  * @copyright      2020 Cubo CMS <https://cubo-cms.com/COPYRIGHT.md>
+  * @license        MIT license <https://cubo-cms.com/LICENSE.md>
+  * @author         Papiando <info@papiando.com>
+  **/
+
+import Cubo from './Namespace.mjs';
+await Cubo.load();
+
+const { Application } = Cubo;
+
+const app = new Application();
+app.success(() => {
+  app.start();
+});
